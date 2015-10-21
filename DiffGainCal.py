17852d2e21a5310dd79780727e0e7a62517d949b
kamenoseiji <seiji.kameno@nao.ac.jp>
2015-10-21 19:45:25 -0300

    modified:   DiffGainCal.py

--- DiffGainCal.py
+++ DiffGainCal.py
@@ -13,15 +13,16 @@ def BP_load( prefix, spw ):
     #
 #
 #BP_ant = BP_load( wd + BPprefix, 10)
-"""
 #-------- Definitions
 antNum = len(refant)
 blNum = antNum* (antNum - 1) / 2 
 spwNum  = len(PointSPW)
 polNum  = len(pol)
+ant0, ant1 = ANT0[0:blNum], ANT1[0:blNum]
 #
 #-------- Procedures for point source
 PointMSfile = wd + PointPrefix + '.ms'
+PlanetMSfile = wd + PlanetPrefix + '.ms'
 antList = GetAntName(PointMSfile)[refant]
 blMap = range(blNum)
 blInv = [False]* blNum		# True -> inverted baseline
@@ -30,8 +31,7 @@ for bl_index in range(blNum):
 	blMap[bl_index], blInv[bl_index]  = Ant2BlD(refant[ants[0]], refant[ants[1]])
 #
 #-------- Procedures
-interval, timeStamp = GetTimerecord(PointMSfile, 0, 0, pol[0], PointSPW[0], PointScan)
-timeNum = len(timeStamp)
+#interval, timeStamp = GetTimerecord(PointMSfile, 0, 0, pol[0], PointSPW[0], PointScan)
 #
 #-------- Prepare Plots
 for ant_index in range(antNum):
@@ -40,119 +40,110 @@ for ant_index in range(antNum):
     figAnt.text(0.45, 0.05, 'MJD [sec]')
     figAnt.text(0.03, 0.45, 'Gain Amplitude and Phase', rotation=90)
 #
-Gain_ant = np.ones([antNum, spwNum, polNum, timeNum], dtype=complex)
-#-------- Baseline-based bandpass
-if BPCAL:
-    BP_bl = np.ones([spwNum, polNum, chNum, blNum], dtype=complex)
-    for bl_index in range(blNum):
-        ants = Bl2Ant(bl_index)
-        BP_bl[:,:,:,bl_index] = BP_ant[ants[0]]* BP_ant[ants[1]].conjugate()
-    #
-#
 #-------- Loop for SPW
-CaledVis = np.zeros([polNum, spwNum, blNum, timeNum], dtype=complex)
 for spw_index in range(spwNum):
-    print ' Loading SPW = ' + `PointSPW[spw_index]`
+    #-------- Baseline-based bandpass
     chNum, chWid, Freq = GetChNum(PointMSfile, PointSPW[spw_index]); Freq = 1.0e-9* Freq  # GHz
     chRange = range(int(round(chNum/chBunch * 0.05)), int(round(chNum/chBunch * 0.95)))
+    BP_bl = np.ones([blNum, polNum, chNum], dtype=complex)
+    if BPCAL:
+        BP_ant = BP_load( wd + BPprefix, PointSPW[spw_index])
+        BP_bl *= BP_ant[ant1].conjugate()
+        BP_bl *= BP_ant[ant0]
+    #
+    #-------- Load Visibilities
+    print ' Loading SPW = ' + `PointSPW[spw_index]`
     timeStamp, Pspec, Xspec = GetVisAllBL(PointMSfile, PointSPW[spw_index], PointScan)
+    timeNum = len(timeStamp)
     Xspec = Xspec[pol]; Xspec = Xspec[:,:,blMap]
+    #
     #-------- Baseline-based cross power spectra
     Ximag = Xspec.transpose(0,1,3,2).imag * (-2.0* np.array(blInv) + 1.0)
     Xspec.imag = Ximag.transpose(0,1,3,2)
+    Xspec = (Xspec.transpose(3,2,0,1) / BP_bl).transpose(2, 3, 1, 0)
     #-------- Tsys calibration
+    TsysBL = np.ones([blNum, polNum, chNum])    # Baseline-based SEFD with Ae=1.0
     if TSYSCAL:
         TrxSP, TsysSP = TsysSpec(PointMSfile, pol, PointTsysScan, PointSPW[spw_index], F)
-        TsysBL = np.ones([polNum, chNum, blNum])
-        for bl_index in range(blNum):
-            ants = Bl2Ant(bl_index)
-            TsysBL[:,:,bl_index] = sqrt( TsysSP[ants[0]]* TsysSP[ants[1]] )
-        #
-        for time_index in range(timeNum):
-            Xspec[:,:,:,time_index] *= TsysBL
-        #
+        TsysBL *= sqrt( TsysSP[ant0] * TsysSP[ant1] )
     #
-    #-------- Delay Determination and calibration
-    if BPCAL:
-        tempVis = np.mean( Xspec.transpose(3,0,1,2) / BP_bl[spw_index], axis=2).transpose(1,2,0)
-    elif chNum == 1:
-        tempVis = Xspec[:,0,:,:]
-    else:
-        tempVis = np.mean(Xspec, axis=1)    # tempVis[pol, ch, bl]
-    #
-    #
+    Xspec = (Xspec.transpose(3,2,0,1) * TsysBL).transpose(2, 3, 1, 0)
+    #-------- Gain Cal
+    Gain_ant = np.ones([antNum, polNum, timeNum], dtype=complex)
     for pol_index in range(polNum):
-        vis_bl  = tempVis[pol_index]
-        Gain_ant[:, spw_index, pol_index] = np.apply_along_axis(gainComplex, 0, vis_bl )
-        CaledVis[pol_index, spw_index] = gainCalVis(vis_bl, Gain_ant[:, spw_index, pol_index], Gain_ant[:, spw_index, pol_index])
+        vis_bl  = np.mean(Xspec[pol_index, chRange], axis=0)
+        Gain_ant[:, pol_index] = np.apply_along_axis(gainComplex, 0, vis_bl )
+        CaledVis = gainCalVis(vis_bl, Gain_ant[:, pol_index], Gain_ant[:, pol_index])
     #
-#
-#-------- Plot Gain
-plotMax = 2.0* np.median(abs(Gain_ant))
-for ant_index in range(antNum):
-    figAnt = plt.figure(ant_index)
-    for pol_index in range(polNum):
-        GAampPL = figAnt.add_subplot( 4, 1, 2* pol_index + 1 )
-        GAphsPL = figAnt.add_subplot( 4, 1, 2* pol_index + 2 )
-        for spw_index in range(spwNum):
-            plotGA = Gain_ant[ant_index, spw_index, pol_index]
+    plotMax = 2.0* np.median(abs(Gain_ant))
+    #-------- Plot Gain
+    for ant_index in range(antNum):
+        figAnt = plt.figure(ant_index)
+        for pol_index in range(polNum):
+            GAampPL = figAnt.add_subplot( 4, 1, 2* pol_index + 1 )
+            GAphsPL = figAnt.add_subplot( 4, 1, 2* pol_index + 2 )
+            plotGA = Gain_ant[ant_index, pol_index]
             GAampPL.plot( timeStamp, abs(plotGA), ls='steps-mid', label = 'SPW=' + `PointSPW[spw_index]`)
             GAphsPL.plot( timeStamp, np.angle(plotGA), '.', label = 'SPW=' + `PointSPW[spw_index]`)
-            text_sd = '%s %s %02d : %5.2f' % (antList[ant_index], polName[pol_index], PointSPW[spw_index], 100.0* np.max(abs(Gain_ant[ant_index, spw_index, pol_index])**2))
+            text_sd = '%s %s %02d : %5.2f' % (antList[ant_index], polName[pol_index], PointSPW[spw_index], 100.0* np.max(abs(Gain_ant[ant_index, pol_index])**2))
             print text_sd
+            if spw_index == spwNum - 1:
+                GAampPL.set_ylim(0.0, 1.25* plotMax )
+                GAphsPL.set_ylim(-math.pi, math.pi)
+                GAampPL.legend(loc = 'upper right', prop={'size' :7}, numpoints = 1)
+                GAphsPL.legend(loc = 'upper right', prop={'size' :7}, numpoints = 1)
+                GAampPL.text( np.min(timeStamp), 1.1* plotMax, polName[pol[pol_index]] + ' Amp')
+                GAphsPL.text( np.min(timeStamp), 2.5, polName[pol[pol_index]] + ' Phase')
+                figAnt.savefig('GA_' + PointPrefix + '_' + antList[ant_index] + '_Scan' + `PointScan` + '.pdf')
+            #
         #
-        GAampPL.set_ylim(0.0, 1.25* plotMax )
-        GAphsPL.set_ylim(-math.pi, math.pi)
-        GAampPL.legend(loc = 'upper right', prop={'size' :7}, numpoints = 1)
-        GAphsPL.legend(loc = 'upper right', prop={'size' :7}, numpoints = 1)
-        GAampPL.text( np.min(timeStamp), 1.1* plotMax, polName[pol[pol_index]] + ' Amp')
-        GAphsPL.text( np.min(timeStamp), 2.5, polName[pol[pol_index]] + ' Phase')
     #
-    figAnt.savefig('GA_' + PointPrefix + '_' + antList[ant_index] + '_Scan' + `PointScan` + '.pdf')
-#
-np.save(PointPrefix + '.Ant.npy', antList) 
-np.save(PointPrefix + 'Scn' + `PointScan` + '.GA.npy', Gain_ant) 
-plt.close('all')
-"""
-"""
 #-------- Visibilities for Planet
-PlanetMSfile = wd + PlanetPrefix + '.ms'
-interval, timeStamp = GetTimerecord(PlanetMSfile, 0, 0, pol[0], PlanetSPW[0], PlanetScan)
-timeNum = len(timeStamp)
-chNum, chWid, Freq = GetChNum(PlanetMSfile, PlanetSPW[0]); Freq = 1.0e-9* Freq  # GHz
-CaledVis = np.zeros([polNum, spwNum, blNum, timeNum], dtype=complex)
-for spw_index in range(spwNum):
     print ' Loading SPW = ' + `PlanetSPW[spw_index]`
-    chRange = range(int(round(chNum/chBunch * 0.05)), int(round(chNum/chBunch * 0.95)))
     timeStamp, Pspec, Xspec = GetVisAllBL(PlanetMSfile, PlanetSPW[spw_index], PlanetScan)
     timeNum = len(timeStamp)
     Xspec = Xspec[pol]; Xspec = Xspec[:,:,blMap]
     #-------- Baseline-based cross power spectra
     Ximag = Xspec.transpose(0,1,3,2).imag * (-2.0* np.array(blInv) + 1.0)
     Xspec.imag = Ximag.transpose(0,1,3,2)
+    Xspec = (Xspec.transpose(3,2,0,1) / BP_bl).transpose(2, 3, 1, 0)
     #-------- Tsys calibration
+    TsysBL = np.ones([blNum, polNum, chNum])    # Baseline-based SEFD with Ae=1.0
     if TSYSCAL:
         TrxSP, TsysSP = TsysSpec(PlanetMSfile, pol, PlanetTsysScan, PlanetSPW[spw_index], F)
-        TsysBL = np.ones([polNum, chNum, blNum])
-        for bl_index in range(blNum):
-            ants = Bl2Ant(bl_index)
-            TsysBL[:,:,bl_index] = sqrt( TsysSP[ants[0]]* TsysSP[ants[1]] )
-        #
-        for time_index in range(timeNum):
-            Xspec[:,:,:,time_index] *= TsysBL
-        #
+        TsysBL *= sqrt( TsysSP[ant0] * TsysSP[ant1] )
     #
-    tempVis = np.mean(Xspec, axis=1)    # tempVis[pol, bl, time]
-    #-------- Gain amplitude calibration
+    Xspec = (Xspec.transpose(3,2,0,1) * TsysBL).transpose(2, 3, 1, 0)
+    #-------- Gain Cal
     for pol_index in range(polNum):
-        GainAmp = np.outer(np.mean( abs(Gain_ant[:, spw_index, pol_index]), axis=1), np.ones([timeNum], dtype=complex))
+        GainAmp = np.outer(np.mean( abs(Gain_ant[:, pol_index]), axis=1), np.ones([timeNum], dtype=complex))
+        vis_bl  = np.mean(Xspec[pol_index, chRange], axis=0)
         for time_index in range(timeNum):
-            antPhase, PhsError = clphase_solve( np.angle(tempVis[pol_index, :, time_index]), abs(tempVis[pol_index, :, time_index]))
+            antPhase, PhsError = clphase_solve( np.angle(vis_bl[:, time_index]), abs(vis_bl[:, time_index]))
             GainAmp[:,time_index] *= exp(1.0j* antPhase)
         #
-        CaledVis[pol_index, spw_index] = gainCalVis(tempVis[pol_index], GainAmp, GainAmp)
+        CaledVis = gainCalVis(vis_bl, GainAmp, GainAmp)
     #
+    #-------- Imaging
+    for pol_index in range(polNum):
+        chNum, chWid, Freq = GetChNum(PlanetMSfile, PlanetSPW[spw_index]); Freq = 1.0e-9* Freq  # GHz
+        lambdaInv = np.mean(Freq)*1.0e9/constants.c     # 1/wavelength [m^-1]
+        timeStamp, UVW = GetUVW(PlanetMSfile, PlanetSPW[spw_index], PlanetScan)    # UVW[3, blNum, timeNum]
+        UVW *= lambdaInv
+        UVmax = sqrt(np.max( UVW[0]**2 + UVW[1]**2 ))
+        UVmin = sqrt(np.min( UVW[0]**2 + UVW[1]**2 ))
+        xi, yi = np.mgrid[ -4*floor(UVmax):4*floor(UVmax):512j, -4*floor(UVmax):4*floor(UVmax):512j]       # (u, v) sampling points
+        U = np.r_[UVW[0], -UVW[0]].reshape(2*blNum*timeNum)
+        V = np.r_[UVW[1], -UVW[1]].reshape(2*blNum*timeNum)
+        reCaledVis = np.r_[ CaledVis[pol_index, spw_index].real,  CaledVis[pol_index, spw_index].real ].reshape(2*blNum*timeNum)
+        imCaledVis = np.r_[ CaledVis[pol_index, spw_index].imag, -CaledVis[pol_index, spw_index].imag ].reshape(2*blNum*timeNum)
 #
+#
+#
+#np.save(PointPrefix + '.Ant.npy', antList) 
+#np.save(PointPrefix + 'Scn' + `PointScan` + '.GA.npy', Gain_ant) 
+#plt.close('all')
+"""
 #-------- Imaging
 #for pol_index in range(polNum):
 #    for spw_index in range(spwNum):
