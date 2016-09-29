beda220726b26b7834014fbf06d15b69ec9737c6
kamenoseiji <seiji.kameno@nao.ac.jp>
2016-09-28 21:01:47 -0300

    modified:   checkGridSurvey.py

--- checkGridSurvey.py
+++ checkGridSurvey.py
@@ -84,9 +84,9 @@ antDia = np.ones([UseAntNum])
 for ant_index in range(UseAntNum): antDia[ant_index] = msmd.antennadiameter(antList[antMap[ant_index]])['value']
 #-------- Check Scans of BandPass, EQualization, and FluxScaling
 try:
-    FCScans = np.r_[msmd.scansforintent("CALIBRATE_FLUX#ON_SOURCE")
+    FCScans = msmd.scansforintent("CALIBRATE_FLUX#ON_SOURCE")
 except:
-    FCSscans = msmd.scansforintent("CALIBRATE_AMPLI#ON_SOURCE")]
+    FCScans = msmd.scansforintent("CALIBRATE_AMPLI#ON_SOURCE")]
 #
 BPScans = msmd.scansforintent("CALIBRATE_BANDPASS#ON_SOURCE")
 ONScans = msmd.scansforintent("CALIBRATE_PHASE#ON_SOURCE")
