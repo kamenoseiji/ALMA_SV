7035ff5956e4d38f3882c9a5a7dbc48892b6a2c3
kamenoseiji <seiji.kameno@nao.ac.jp>
2017-06-28 23:35:51 -0400

    modified:   checkGain.py

--- checkGain.py
+++ checkGain.py
@@ -19,7 +19,7 @@ print text_sd
 blMap, blInv= range(UseBlNum), [False]* UseBlNum
 ant0, ant1 = ANT0[0:UseBlNum], ANT1[0:UseBlNum]
 for bl_index in range(UseBlNum): blMap[bl_index] = Ant2Bl(UseAnt[ant0[bl_index]], UseAnt[ant1[bl_index]])
-timeStamp, UVW = GetUVW(msfile, spw[0], msmd.scansforspw(spw[0])[0])
+timeStamp, UVW = GetUVW(msfile, spw, msmd.scansforspw(spw)[0])
 uvw = np.mean(UVW[:,blMap], axis=2); uvDist = np.sqrt(uvw[0]**2 + uvw[1]**2)
 if 'refant' in locals():    refantID = indexList(np.array([refant]), antList)[0]
 else: refantID = bestRefant(uvDist)
