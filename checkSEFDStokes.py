8a3fe34e47a59cbac0c5bb6345ed4b4e4ba876e0
kamenoseiji <seiji.kameno@nao.ac.jp>
2018-08-14 11:51:17 +0000

    modified:   aprioriStokes.py 	modified:   checkSEFDStokes.py

--- checkSEFDStokes.py
+++ checkSEFDStokes.py
@@ -186,7 +186,7 @@ QCpUS = (QUsolution[0]* np.cos(2.0* PA) + QUsolution[1]* np.sin(2.0* PA)) / cata
 #useAntMapRev = indexList(np.array(antMap), np.array(UseAnt))
 exTauSP = []
 for spw_index in range(spwNum):
-    exTauSP = exTauSP + [UnivariateSpline(atmTimeRef, Tau0E[spw_index], np.ones(len(atmTimeRef)), s=0.01*np.std(Tau0E[spw_index]))]
+    exTauSP = exTauSP + [UnivariateSpline(atmTimeRef, Tau0E[spw_index], np.ones(len(atmTimeRef)), s=0.1*np.std(Tau0E[spw_index]))]
     exp_Tau = np.exp(-(Tau0spec[spw_index] + exTauSP[spw_index](np.median(timeStamp))) / np.mean(np.sin(ElScan)))
     TsysEQScan = np.mean(Trxspec[spw_index,:,:,chRange].transpose(1,2,0) + Tcmb*exp_Tau[chRange] + tempAtm* (1.0 - exp_Tau[chRange]), axis=2)[useAntMap] # [antMap, pol]
     #-------- Baseline-based cross power spectra
