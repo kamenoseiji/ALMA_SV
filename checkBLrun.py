72c616f8fb57967594908b6e694f5a2a5ef57837
Seiji Kameno <seiji.kameno@nao.ac.jp>
2018-08-16 11:39:09 -0300

    modified:   checkBLrun.py 	modified:   checkGridSurvey.py

--- checkBLrun.py
+++ checkBLrun.py
@@ -9,7 +9,7 @@ class END(Exception):
 #
 #-------- Procedures
 msfile = wd + prefix + '.ms'
-#execfile(SCR_DIR + 'TsysCal.py')
+execfile(SCR_DIR + 'TsysCal.py')
 #-------- Check Antenna List
 antList = GetAntName(msfile)
 antNum = len(antList)
@@ -18,22 +18,21 @@ blNum = antNum* (antNum - 1) / 2
 msmd.open(msfile)
 print prefix
 print '---Checking spectral windows'
-if not 'spwList' in locals():
-    spwList = list(set(msmd.tdmspws()) & set(msmd.spwsforintent("CALIBRATE_ATMOSPHERE*"))); spwList.sort()
-#
-if not 'spwNames' in locals(): spwNames = msmd.namesforspws(spwList)
-if len(spwList) < 1: sys.exit('No available SPWs found.\n')
-#
-BandNames, pattern = [], r'RB_..'
-for spwName in spwNames: BandNames = BandNames + re.findall(pattern, spwName)
-UniqBands = unique(BandNames).tolist(); NumBands = len(UniqBands)
-spwLists, BandScans, BandPA = [], [], []
+atmSPWs = list(set(msmd.tdmspws()) & set(msmd.spwsforintent("CALIBRATE_ATMOSPHERE*"))); atmSPWs.sort()
+bpSPWs  = list(set( msmd.tdmspws()) & set(msmd.spwsforintent("CALIBRATE_DELAY*"))); bpSPWs.sort()
+atmspwNames, bpspwNames = msmd.namesforspws(atmSPWs), msmd.namesforspws(bpSPWs)
+atmBandNames, atmPattern = [], r'RB_..'
+for spwName in atmspwNames : atmBandNames = atmBandNames + re.findall(atmPattern, spwName)
+UniqBands = unique(atmBandNames).tolist(); NumBands = len(UniqBands)
+atmspwLists, bpspwLists, atmscanLists, bpscanLists, BandPA = [], [], [], [], []
 for band_index in range(NumBands):
     BandPA = BandPA + [(BANDPA[int(UniqBands[band_index][3:5])] + 90.0)*pi/180.0]
-    spwLists = spwLists + [np.array(spwList)[indexList( np.array([UniqBands[band_index]]), np.array(BandNames))].tolist()]
-    BandScans = BandScans + [msmd.scansforspw(spwLists[band_index][0])]
+    atmspwLists = atmspwLists + [np.array(atmSPWs)[indexList(np.array([UniqBands[band_index]]), np.array(atmBandNames))].tolist()]
+    bpspwLists  = bpspwLists  + [np.array(bpSPWs)[indexList( np.array([UniqBands[band_index]]), np.array(atmBandNames))].tolist()]
+    atmscanLists= atmscanLists+ [list(set(msmd.scansforintent("CALIBRATE_ATMOSPHERE*")) & set(msmd.scansforspw(atmspwLists[band_index][0])))]
+    bpscanLists = bpscanLists + [msmd.scansforspw(bpspwLists[band_index][0]).tolist()]
     print ' ',
-    print UniqBands[band_index] + ': SPW=' + `spwLists[band_index]`
+    print UniqBands[band_index] + ': atmSPW=' + `atmspwLists[band_index]` + ' bpSPW=' + `bpspwLists[band_index]`
 #
 #-------- Check source list
 print '---Checking source list'
@@ -41,7 +40,7 @@ sourceList, posList = GetSourceList(msfile); sourceList = sourceRename(sourceLis
 for source in sourceList: print source,
 print ''
 #-------- Check Scans of BandPass, EQualization, and FluxScaling
-polNum = msmd.ncorrforpol(msmd.polidfordatadesc(spwList[0]))
+polNum = msmd.ncorrforpol(msmd.polidfordatadesc(bpspwLists[0][0]))
 ONScans = msmd.scansforintent("CALIBRATE_PHASE#ON_SOURCE")
 print '---SPWs and Scans for each receiver band'
 msmd.done()
@@ -49,9 +48,10 @@ for band_index in range(NumBands):
     bandName = UniqBands[band_index]; bandID = int(UniqBands[band_index][3:5])
     msmd.open(msfile)
     #-------- Check Calibrators
-    ONScan = BandScans[band_index][indexList( ONScans, BandScans[band_index] )]
-    onsourceScans = ONScan.tolist()
-    scanNum = len(onsourceScans)
+    ONScan = np.array(bpscanLists[band_index])[indexList( ONScans, np.array(bpscanLists[band_index]))]
+    ATMScan= np.array(atmscanLists[band_index])[indexList( ONScans, np.array(atmscanLists[band_index]))]
+    onsourceScans, atmScans = ONScan.tolist(), ATMScan.tolist()
+    scanNum, atmscanNum = len(onsourceScans), len(atmScans)
     #-------- Check AZEL
     azelTime, AntID, AZ, EL = GetAzEl(msfile)
     azelTime_index = np.where( AntID == 0 )[0].tolist() 
@@ -74,7 +74,7 @@ for band_index in range(NumBands):
     #
     for scan_index in range(scanNum):
         sourceIDscan.append( msmd.sourceidforfield(msmd.fieldsforscan(onsourceScans[scan_index])[0]))
-        interval, timeStamp = GetTimerecord(msfile, 0, 0, spwLists[band_index][0], onsourceScans[scan_index])
+        interval, timeStamp = GetTimerecord(msfile, 0, 0, bpspwLists[band_index][0], onsourceScans[scan_index])
         AzScan, ElScan = AzElMatch(timeStamp, azelTime, AntID, 0, AZ, EL)
         PA = AzEl2PA(AzScan, ElScan) + BandPA[band_index]; dPA = np.std(np.sin(PA)) #dPA = abs(np.sin(max(PA) - min(PA)))
         OnAZ.append(np.median(AzScan)); OnEL.append(np.median(ElScan)); OnPA.append(np.median(PA))
@@ -106,7 +106,9 @@ for band_index in range(NumBands):
     BPcalText = 'Use %s [EL = %4.1f deg] as Bandpass Calibrator' % (BPcal, 180.0* OnEL[onsourceScans.index(BPScan)]/np.pi); print BPcalText
     EQcalText = 'Use %s [EL = %4.1f deg] as Gain Equalizer' % (EQcal, 180.0* OnEL[onsourceScans.index(EQScan)]/np.pi); print EQcalText
     #-------- Polarization setup
+    atmspw = atmspwLists[band_index]; spwNum = len(atmspw)
     scnspw = bpspwLists[band_index]; scnspwNum = len(scnspw)
+    #scnspw = bpspwLists[band_index]; scnspwNum = len(scnspw)
     #spwList = spwLists[band_index]; spwNum = len(spwList)
     pPol, cPol = [0,1], []  # parallel and cross pol
     PolList = ['X', 'Y']
--- checkGridSurvey.py
+++ checkGridSurvey.py
@@ -91,8 +91,8 @@ for band_index in range(NumBands):
             EQquality = EQquality + [catalogIQUV[0]**2 * np.sin(OnEL[scan_index] - ELshadow) / (1.0e-4 + abs(QCpUS))]
         else:
             QCpUS, UCmQS = 0.0, 0.0
-            BPquality = BPquality + [-1.0]
-            EQquality = EQquality + [-1.0]
+            BPquality = BPquality + [-9999.9]
+            EQquality = EQquality + [-9999.9]
         #
         print 'Scan%02d : %10s AZ=%6.1f EL=%4.1f PA=%6.1f dPA=%5.2f pRes=%5.2f BPquality=%7.4f EQquality=%6.0f' % (onsourceScans[scan_index], sourceList[sourceIDscan[scan_index]], 180.0*OnAZ[scan_index]/np.pi, 180.0*OnEL[scan_index]/np.pi, 180.0*OnPA[scan_index]/np.pi, 180.0*dPA/np.pi, UCmQS, BPquality[scan_index], EQquality[scan_index]) 
         if sourceIDscan[scan_index] in SSOList: FLscore[scan_index] = np.exp(np.log(math.sin(OnEL[scan_index])-0.34))* SSOscore[bandID-1][SSOCatalog.index(sourceList[sourceIDscan[scan_index]])]
