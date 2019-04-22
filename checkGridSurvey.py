9702ec4e2a94af9824f9a3cbb384f6b9544133ff
Seiji Kameno <kamenoseiji@mac.com>
2019-04-22 18:24:21 -0400

    modified:   checkGridSurvey.py

--- checkGridSurvey.py
+++ checkGridSurvey.py
@@ -161,7 +161,7 @@ for band_index in range(NumBands):
             execfile(SCR_DIR + 'aprioriFlux.py')
     #
 #
-del msfile, UniqBands, UseAnt
+del msfile, UniqBands, UseAnt, atmspwLists
 if 'flagAnt' in locals(): del flagAnt
 if 'BPScans' in locals(): del BPScans
 if 'EQScans' in locals(): del EQScans
