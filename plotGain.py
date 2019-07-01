a9891b7927896e37bf1f24599ee7a9cd5a9864b5
Seiji Kameno <seiji.kameno@nao.ac.jp>
2019-07-01 16:47:40 +0000

    modified:   checkGridSurvey.py 	modified:   plotGain.py

--- plotGain.py
+++ plotGain.py
@@ -22,8 +22,10 @@ for ant_index in range(antNum):
     PhsPL = figPhs.add_subplot( int(np.ceil(antNum/2.0)), 2, ant_index + 1 )
     #PhsPL = figAnt.add_subplot( antNum, 1, ant_index + 1 )
     #plotX, plotY = Gain[ant_index, 0], Gain[ant_index, 0]
-    for pol_index in range(2): AmpPL.plot( timeStamp, abs(Gain[ant_index, pol_index]), ls='steps-mid')
+    #for pol_index in range(2): AmpPL.plot( timeStamp, abs(Gain[ant_index, pol_index]), ls='steps-mid')
+    for pol_index in range(2): AmpPL.plot( timeStamp, abs(Gain[ant_index, pol_index]), '.')
     for pol_index in range(2): PhsPL.plot( timeStamp, np.angle(Gain[ant_index, pol_index]), '.')
+    ampMed = np.median(abs(Gain[ant_index]), axis=1)
     AmpPL.yaxis.set_major_formatter(ptick.ScalarFormatter(useMathText=True))
     AmpPL.yaxis.offsetText.set_fontsize(3)
     PhsPL.yaxis.offsetText.set_fontsize(3)
@@ -35,7 +37,8 @@ for ant_index in range(antNum):
     #
     #AmpPL.legend(loc = 'lower left', prop={'size' :7}, numpoints = 1)
     #PhsPL.legend(loc = 'best', prop={'size' :7}, numpoints = 1)
-    AmpPL.text( np.min(timeStamp), 0.8* plotMin + 0.2* plotMax, antList[ant_index], fontsize=5)
+    text_sd = '%s : Gain(median) = (%.2f%% %.2f%%)' % (antList[ant_index], 100.0* ampMed[0], 100.0* ampMed[1])
+    AmpPL.text( np.min(timeStamp), 0.8* plotMin + 0.2* plotMax, text_sd, fontsize=5)
     PhsPL.text( np.min(timeStamp), -0.5*pi, antList[ant_index], fontsize=5)
 #
 figAmp.savefig(pp, format='pdf')
