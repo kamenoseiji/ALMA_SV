a41cdcbab1f029bc30fbbbb7df25ecc3863d8ebb
Seiji Kameno <seiji.kameno@nao.ac.jp>
2019-07-21 21:31:57 +0000

    modified:   polQuery.R

--- polQuery.R
+++ polQuery.R
@@ -1,5 +1,5 @@
 Arguments <- commandArgs(trailingOnly = T)
-timeWindow <- 120	# Days
+timeWindow <- 90	# Days
 #-------- Function to return residuals in RM fit
 residEVPA <- function(x, y, w){	# Optimization function for RM fit
 	return(function(para){
